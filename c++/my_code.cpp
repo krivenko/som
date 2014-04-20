963e55d0ef7c37fdc55b63914a474926ee9f4f69
Igor Krivenko <igor.s.krivenko@gmail.com>
2014-04-20 23:58:44 +0200

    Initial commit

---
+++ c++/my_code.cpp
@@ -0,0 +1,12 @@
+#include "./my_code.hpp"
+
+my_class::my_class(triqs::utility::parameters p) {
+ Beta = p["beta"];
+ Arr = triqs::utility::extract<decltype(Arr)>(p["Arr"]);
+ auto g1_x = gf<imtime>{{Beta, Fermion, 1000}, {1, 1}};
+ g1 = make_block_gf<imtime>(2, g1_x);
+ g2 = gf<refreq>{{-10, 10, 1000}, {1, 1}};
+ g3 = gf<imtime>{{Beta, Fermion, 1000}, {1, 1}};
+ ar = {0,1,2,3};
+ m1 = matrix<double>{{0, 1}, {2, 3}};
+}
