bfd286fbcc214efd3caccd2afe5b8520c93527f4
Igor Krivenko <igor.s.krivenko@gmail.com>
2016-05-28 17:40:27 +0200

    Updated GPL headers in C++ and Python sources
    * Changed application version to 0.7.
    * Updated LICENSE.txt.
    * Temporarily disabled gf and python_gf tests.

--- c++/config_update.hpp
+++ c++/config_update.hpp
@@ -1,21 +1,21 @@
 /*******************************************************************************
  *
- * TRIQS: a Toolbox for Research in Interacting Quantum Systems
+ * SOM: Stochastic Optimization Method for Analytic Continuation
  *
  * Copyright (C) 2016 by I. Krivenko
  *
- * TRIQS is free software: you can redistribute it and/or modify it under the
+ * SOM is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * TRIQS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * SOM is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
  * You should have received a copy of the GNU General Public License along with
- * TRIQS. If not, see <http://www.gnu.org/licenses/>.
+ * SOM. If not, see <http://www.gnu.org/licenses/>.
  *
  ******************************************************************************/
 
--- test/c++/gf.cpp
+++ test/c++/gf.cpp
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ *
+ * SOM: Stochastic Optimization Method for Analytic Continuation
+ *
+ * Copyright (C) 2016 by I. Krivenko
+ *
+ * SOM is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * SOM is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * SOM. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
 #include <triqs/test_tools/arrays.hpp>
 
 #include "som_core.hpp"
