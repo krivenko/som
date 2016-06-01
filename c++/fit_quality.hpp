d6303cc9081347b8a55dbc8d49968e8143451677
Igor Krivenko <igor.s.krivenko@gmail.com>
2016-06-01 11:50:18 +0200

    [fit_quality] Do not quantize contributions to \kappa in the complex case

--- c++/fit_quality.hpp
+++ c++/fit_quality.hpp
@@ -46,12 +46,13 @@ class fit_quality {
 
  // Correlation measure of two real numbers
  static double corr(double x1, double x2) {
-  return x1*x2;
+  return (x1*x2 < 0) ? 1 : 0;
  }
 
  // Correlation measure of two complex numbers
  static double corr(dcomplex z1, dcomplex z2) {
-  return std::real(z1 * std::conj(z2));
+  dcomplex w = z1 * std::conj(z2);
+  return 0.5*(1.0 - (w == .0 ? 1.0 : (w.real() / std::abs(w))));
  }
 
 public:
@@ -65,9 +66,7 @@ public:
   auto delta = (rhs - kern(c)) / error_bars;
   int M = first_dim(delta);
   double kappa = 0;
-  for(int i = 1; i < M; ++i) {
-   if(corr(delta(i), delta(i-1)) < 0) kappa += 1;
-  }
+  for(int i = 1; i < M; ++i) kappa += corr(delta(i), delta(i-1));
   kappa /= M - 1;
   return kappa;
  }
