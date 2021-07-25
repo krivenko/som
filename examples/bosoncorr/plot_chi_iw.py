39c0e251e68463b572574e93a34af805e51362a7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-25 18:16:11 +0000

    Deploy krivenko/som to github.com/krivenko/som.git:gh-pages

---
+++ examples/bosoncorr/plot_chi_iw.py
@@ -0,0 +1,28 @@
+from pytriqs.gf.local import *
+from pytriqs.archive import HDFArchive
+from matplotlib import pyplot as plt
+from pytriqs.plot.mpl_interface import oplot
+
+# Read data from archive
+ar = HDFArchive('results.h5', 'r')
+
+# Plot input and reconstructed \chi(i\omega_n), real parts
+oplot(ar['chi_iw'][0,0],     mode='R', linewidth=0.8, label="$\chi'_0(i\\omega_n)$")
+oplot(ar['chi_rec_iw'][0,0], mode='R', linewidth=0.8, label="$\chi'_\mathrm{0,rec}(i\\omega_n)$")
+oplot(ar['chi_iw'][1,1],     mode='R', linewidth=0.8, label="$\chi'_1(i\\omega_n)$")
+oplot(ar['chi_rec_iw'][1,1], mode='R', linewidth=0.8, label="$\chi'_\mathrm{1,rec}(i\\omega_n)$")
+
+plt.xlim((0, 20))
+plt.ylabel("$\chi'(i\\omega_n)$")
+plt.legend(loc="upper center")
+plt.show()
+
+# Plot input and reconstructed \chi(i\omega_n), imaginary parts
+oplot(ar['chi_iw'][0,0],     mode='I', linewidth=0.8, label="$\chi''_0(i\\omega_n)$")
+oplot(ar['chi_rec_iw'][0,0], mode='I', linewidth=0.8, label="$\chi''_\mathrm{0,rec}(i\\omega_n)$")
+oplot(ar['chi_iw'][1,1],     mode='I', linewidth=0.8, label="$\chi''_1(i\\omega_n)$")
+oplot(ar['chi_rec_iw'][1,1], mode='I', linewidth=0.8, label="$\chi''_\mathrm{1,rec}(i\\omega_n)$")
+
+plt.xlim((0, 20))
+plt.ylabel("$\chi(i\\omega_n)$")
+plt.legend(loc="lower center")
