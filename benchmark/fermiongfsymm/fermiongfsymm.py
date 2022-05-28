67fe7c30c38f0ae34ce68beb3fd8c89be5963c1f
Igor Krivenko <igor.s.krivenko@gmail.com>
2022-05-28 15:50:55 +0200

    [benchmark] New benchmark `fermiongfsymm`
    Its goal is to compare results for the same symmetric model spectrum
    obtained via `FermionGf` and `FermionGfSymm` kernels.

---
+++ benchmark/fermiongfsymm/fermiongfsymm.py
@@ -0,0 +1,116 @@
+##############################################################################
+#
+# SOM: Stochastic Optimization Method for Analytic Continuation
+#
+# Copyright (C) 2016-2022 Igor Krivenko <igor.s.krivenko@gmail.com>
+#
+# SOM is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# SOM is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# SOM. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+#
+# Comparison of results for the same symmetric model spectrum obtained via
+# FermionGf and FermionGfSymm kernels.
+#
+
+import time
+from h5 import HDFArchive
+from triqs.gf import *
+from triqs.gf.descriptors import *
+import triqs.utility.mpi as mpi
+from som import Som, fill_refreq, reconstruct, compute_tail
+from som.version import som_hash
+from scipy.integrate import quad
+import numpy as np
+
+def print_master(msg):
+    if mpi.rank == 0: print(msg)
+    mpi.barrier()
+
+#
+# Parameters
+#
+
+beta = 10.0
+
+n_tau = 201
+n_iw = 201
+n_w = 1001
+tail_max_order = 10
+energy_window = (-5, 5)
+
+def g_tau_model_symm(tau):
+    dos = lambda e: np.sqrt(4 - e**2) / (2 * np.pi)
+    kern = lambda e: -np.exp(-tau * e) / (1 + np.exp(-beta*e))
+    return quad(lambda e: dos(e) * kern(e), -2, 2, points = [0])[0]
+
+norms = [1.0]
+
+abs_error = 1e-4
+np.random.seed(1000)
+
+run_params = {}
+run_params['verbosity'] = 2
+run_params['adjust_l'] = False
+run_params['t'] = 100
+run_params['f'] = 100
+run_params['l'] = 2000
+run_params['make_histograms'] = False
+
+if mpi.is_master_node():
+    arch = HDFArchive('fermiongfsymm.h5', 'w')
+    arch["som_git_hash"] = som_hash
+    arch["run_params"] = run_params
+
+def run_som_and_save(kind, g, error_bars, norms, energy_window):
+    cont = Som(g, error_bars, kind = kind, norms = norms)
+    cont.accumulate(energy_window = energy_window, **run_params)
+    cont.compute_final_solution()
+
+    g_w = GfReFreq(window = energy_window, n_points = n_w, indices = [0])
+    fill_refreq(g_w, cont)
+
+    g_rec = g.copy()
+    reconstruct(g_rec, cont)
+
+    tail = compute_tail(tail_max_order, cont)
+
+    if mpi.is_master_node():
+        arch.create_group(kind)
+        gr = arch[kind]
+        gr["input"] = g
+        gr["output"] = g_w
+        gr["rec"] = g_rec
+        gr["tail"] = tail
+        gr["solutions"] = cont.solutions
+
+g_symm_tau = GfImTime(beta = beta, n_points = n_tau, indices = [0])
+
+g_symm_tau << Function(g_tau_model_symm)
+g_symm_tau.data[:] += abs_error * 2 * (np.random.rand(n_tau, 1, 1) - 0.5)
+
+error_bars_tau = g_symm_tau.copy()
+error_bars_tau.data[:] = abs_error
+
+print_master("================")
+print_master("FermionGf kernel")
+print_master("================")
+
+run_som_and_save("FermionGf", g_symm_tau, error_bars_tau, norms, (-5, 5))
+
+print_master("====================")
+print_master("FermionGfSymm kernel")
+print_master("====================")
+
+run_som_and_save("FermionGfSymm", g_symm_tau, error_bars_tau, norms, (-5, 5))
---
+++ benchmark/fermiongfsymm/plot.py
@@ -0,0 +1,74 @@
+##############################################################################
+#
+# SOM: Stochastic Optimization Method for Analytic Continuation
+#
+# Copyright (C) 2016-2022 Igor Krivenko <igor.s.krivenko@gmail.com>
+#
+# SOM is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# SOM is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# SOM. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from h5 import *
+from triqs.gf import *
+from matplotlib import pyplot as plt
+from triqs.plot.mpl_interface import oplot
+from matplotlib.backends.backend_pdf import PdfPages
+from som import Configuration
+
+arch = HDFArchive('fermiongfsymm.h5', 'r')
+pp = PdfPages('fermiongfsymm.pdf')
+
+g_tau = arch["FermionGf"]["input"]
+g_w = arch["FermionGf"]["output"]
+g_w_symm = arch["FermionGfSymm"]["output"]
+g_rec_tau = arch["FermionGf"]["rec"]
+g_rec_tau_symm = arch["FermionGfSymm"]["rec"]
+
+tail = arch["FermionGf"]["tail"]
+tail_symm = arch["FermionGfSymm"]["tail"]
+
+oplot(g_w[0, 0], mode = 'R', lw = 0.5, label = "FermionGf, Re")
+oplot(g_w[0, 0], mode = 'I', lw = 0.5, label = "FermionGf, Im")
+oplot(g_w_symm[0, 0], mode = 'R', lw = 0.5, label = "FermionGfSymm, Re")
+oplot(g_w_symm[0, 0], mode = 'I', lw = 0.5, label = "FermionGfSymm, Im")
+
+w_mesh = list(map(float, g_w.mesh))
+plt.xlim((w_mesh[0], w_mesh[-1]))
+plt.xlabel("$\\omega$")
+plt.ylabel("$G(\\omega)$")
+plt.legend()
+pp.savefig(plt.gcf())
+plt.clf()
+
+plt.plot(tail.flatten().real, lw = 0.5, label = "tail coefficients, FermionGf")
+plt.plot(tail_symm.flatten().real, lw = 0.5,
+         label = "tail coefficients, FermionGfSymm")
+plt.xlim((0, len(tail) - 1))
+plt.xlabel("order")
+plt.legend()
+pp.savefig(plt.gcf())
+plt.clf()
+
+oplot(g_tau[0, 0], mode = 'R', lw = 0.5, label = "input")
+oplot(g_rec_tau[0, 0], mode = 'R', lw = 0.5, label = "rec, FermionGf")
+oplot(g_rec_tau_symm[0, 0], mode = 'R', lw = 0.5, label = "rec, FermionGfSymm")
+
+tau_mesh = list(map(float, g_tau.mesh))
+plt.xlim((tau_mesh[0], tau_mesh[-1]))
+plt.xlabel("$\\tau$")
+plt.ylabel("$G(\\tau)$")
+plt.legend()
+pp.savefig(plt.gcf())
+
+pp.close()
