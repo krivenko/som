1adddb9423f74c83b06eedbc4b690b98816f7181
Igor Krivenko <igor.s.krivenko@gmail.com>
2024-01-16 19:43:36 +0100

    Added shell script that updates copyright years in GPL headers

---
+++ share/update-copyright-years.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+#
+# Update copyright years in files
+#
+
+FIND=$(which find)
+SED=$(which sed)
+
+# Collect paths of files to be updated
+FILES="README.md LICENSE.txt"
+
+for PATTERN in "*.cpp" "*.hpp" "*.hxx" "*.py" "*.in" "CMakeLists.txt" "*.cmake"
+do
+    FILES="$FILES $($FIND . -name $PATTERN)"
+done
+
+# Write the new year string
+YEARS_REGEX="([0-9]{4})-[0-9]{4}"
+YEARS_UPDATED="\\1-$(date +%Y)"
+
+COPYRIGHT_REGEX="Copyright \(C\) ${YEARS_REGEX}"
+COPYRIGHT_UPDATED="Copyright \(C\) ${YEARS_UPDATED}"
+for FILE in $FILES
+do
+    $SED -i -E "s/${COPYRIGHT_REGEX}/${COPYRIGHT_UPDATED}/g" $FILE
+done
+
+# doc/conf.py.in requires special treatment
+sed -i -E "s/${YEARS_REGEX}, Igor Krivenko/${YEARS_UPDATED}, Igor Krivenko/g" \
+       "doc/conf.py.in"
